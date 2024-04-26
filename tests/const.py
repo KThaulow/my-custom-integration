f8f774a039c80d3af6c1cf6599a2bfbc0bf9e648
Kristian Thaulow <kristian1990@gmail.com>
2024-04-26 08:54:38 +0200

    Initial

---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for My Custom Integration tests."""
+from custom_components.my_custom_integration.const import (
+    CONF_PASSWORD,
+)
+from custom_components.my_custom_integration.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
